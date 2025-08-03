d95a40ddd2ed301b080f1674bb0f02e96f615ff3
Brenda Martins <brendamichele16@hotmail.com>
2025-08-03 11:41:38 -0300

    creting usecase to process transaction

--- usecase/process_transaction.go
+++ usecase/process_transaction.go
@@ -1,6 +1,11 @@
 package usecase
 
-import "github.com/brenda-martins/code-bank-golang/domain"
+import (
+	"time"
+
+	"github.com/brenda-martins/code-bank-golang/domain"
+	"github.com/brenda-martins/code-bank-golang/dto"
+)
 
 type UseCaseTransaction struct {
 	TransactionRepository domain.TransactionRepository
@@ -9,4 +14,46 @@ type UseCaseTransaction struct {
 
 func NewUseCaseTransaction(transactionRepository domain.TransactionRepository, creditCardRepository domain.CreditCardRepository) UseCaseTransaction {
 	return UseCaseTransaction{TransactionRepository: transactionRepository, CreditCardRepository: creditCardRepository}
+}
+
+func (u UseCaseTransaction) ProcessTransaction(transactionDto dto.Transaction) (domain.Transaction, error) {
+	creditCard := hydrateCreditCard(transactionDto)
+	ccBalanceAndLimit, err := u.CreditCardRepository.GetCreditCard(*creditCard)
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+
+	creditCard.ID = ccBalanceAndLimit.ID
+	creditCard.Limit = ccBalanceAndLimit.Limit
+	creditCard.Balance = ccBalanceAndLimit.Balance
+
+	t := hydrateTransaction(transactionDto, ccBalanceAndLimit)
+	t.ProcessAndValidate(creditCard)
+
+	err = u.TransactionRepository.SaveTransaction(*t, *creditCard)
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+
+	return *t, nil
+}
+
+func hydrateCreditCard(transactionDto dto.Transaction) *domain.CreditCard {
+	creditCard := domain.NewCreditCard()
+	creditCard.Name = transactionDto.Name
+	creditCard.Number = transactionDto.Number
+	creditCard.ExpirationMonth = transactionDto.ExpirationMonth
+	creditCard.ExpirationYear = transactionDto.ExpirationYear
+	creditCard.CVV = transactionDto.CVV
+	return creditCard
+}
+
+func hydrateTransaction(transaction dto.Transaction, cc domain.CreditCard) *domain.Transaction {
+	t := domain.NewTransaction()
+	t.CreditCardId = cc.ID
+	t.Amount = transaction.Amount
+	t.Store = transaction.Store
+	t.Description = transaction.Description
+	t.CreatedAt = time.Now()
+	return t
 }
